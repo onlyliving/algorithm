d31e1482250bc24ab93dc34b3d55d4882ea45bb9
soheekim <greensohee88@naver.com>
2024-08-17 20:55:09 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.33 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/README.md
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/README.md
@@ -1,10 +1,10 @@
 # [level 1] [PCCP 기출문제] 1번 / 붕대 감기 - 250137 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/250137) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/250137#) 
 
 ### 성능 요약
 
-메모리: 38.6 MB, 시간: 34.19 ms
+메모리: 33.5 MB, 시간: 0.33 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 17일 19:56:41
+2024년 08월 17일 20:55:07
 
 ### 문제 설명
 
--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
@@ -7,36 +7,27 @@ function solution(bandage, health, attacks) {
     let successStreak = 0;
     
     for (let i = 1; i <= attacksTotalTime; i ++) {
-         console.log(i);
-        
-        
         if (attacksSeconds.indexOf(i) !== -1) {
             // 공격받음
             successStreak = 0;
             const minus = attacks[attacksSeconds.indexOf(i)][1];
             curHealth -= minus;
-            console.log("공격받음", i, minus, curHealth);
         } else {
             // 회복
             successStreak ++;
             curHealth += recoveryAmountPerSecond;
-             console.log("회복", i, curHealth);
-
-            if (successStreak === (castingTime)) {
+            if (successStreak === castingTime) {
                  curHealth +=  additionalRecoveryAmount;
                  successStreak = 0;
-                console.log("회복 + 추가회복", i, curHealth);
             }
             
             if (curHealth > health) {
                 curHealth = health;
-                console.log("회복이 기본체력 넘어가면 초기화", i, curHealth);
             }
 
         }
         
         if (curHealth <= 0) {
-            console.log("체력이 0!! die", i);
             return curHealth = -1;
         }
         
